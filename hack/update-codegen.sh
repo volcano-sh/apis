5be4514c62ef43c41cc90f6ae8e2875cc03dd97c
shinytang6 <1074461480@qq.com>
2021-03-24 23:59:30 +0800

    feat(apis): improve infra code
    Signed-off-by: shinytang6 <1074461480@qq.com>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -6,8 +6,7 @@ set -o pipefail
 
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ${GOPATH}/src/k8s.io/code-generator)}
-
-vendor/k8s.io/code-generator/generate-groups.sh "deepcopy,client,informer,lister" \
+bash $CODEGEN_PKG/generate-groups.sh "deepcopy,client,informer,lister" \
   volcano.sh/apis/pkg/client volcano.sh/apis/pkg/apis \
   batch:v1alpha1 bus:v1alpha1 scheduling:v1beta1 \
   --go-header-file ${SCRIPT_ROOT}/hack/boilerplate.go.txt
